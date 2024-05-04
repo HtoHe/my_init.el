b4b52a097422336794ae5740ae29f8f5b3361855
Barrett <illadetagomago@gmail.com>
2024-05-04 16:11:27 +0900

    first commit for windows

--- init.el
+++ init.el
@@ -7,6 +7,9 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
+(setq my-package-list '(flycheck-pos-tip company-c-headers evil-easymotion use-package helm flycheck magit avy undo-tree gnu-elpa-keyring-update company zenburn-theme evil))
+(mapc #'package-install my-package-list)
+
 (load-theme 'zenburn t)
 
 (add-hook 'after-init-hook 'global-company-mode)
@@ -32,16 +35,6 @@
 
 (with-eval-after-load 'flycheck
   (flycheck-pos-tip-mode))
-
-(pdf-tools-install)  ; Standard activation command
-(pdf-loader-install) ; On demand loading, leads to faster startup time
-
-(use-package pdf-view-restore
-  :after pdf-tools
-  :config
-  (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode))
-
-(setq pdf-view-restore-filename "~/.emacs.d/.pdf-view-restore")
 
  ;; custom setting(ui)
 (add-to-list 'default-frame-alist
@@ -74,7 +67,7 @@
  ;; If there is more than one, they won't work right.
  '(evil-undo-system 'undo-tree)
  '(package-selected-packages
-   '(pdf-view-restore pdf-tools flycheck-pos-tip company-c-headers evil-easymotion use-package helm flycheck magit avy undo-tree gnu-elpa-keyring-update company zenburn-theme evil)))
+   '(flycheck-pos-tip company-c-headers evil-easymotion use-package helm flycheck magit avy undo-tree gnu-elpa-keyring-update company zenburn-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
